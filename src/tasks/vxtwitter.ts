23af5c86226c46feae459366ba4c90b7be009836
Kyunoxic <37454616+Kyunoxic@users.noreply.github.com>
2023-09-12 22:37:53 +0200

    Update to support X

--- src/tasks/vxtwitter.ts
+++ src/tasks/vxtwitter.ts
@@ -3,18 +3,28 @@ import { Message } from "discord.js";
 export async function vxTwitterHandler(message: Message) {
     if (message.author.bot) return;
 
+    await message.fetch();
+    let workingURL;
+
     //Check if message includes a twitter URL
     const twitterURL = message.content.match(/https:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/[0-9]+/);
-    if (twitterURL?.length != 1) return;
+    const xURL = message.content.match(/https:\/\/x\.com\/[a-zA-Z0-9_]+\/status\/[0-9]+/);
+    if (twitterURL?.length == 1) {
+        workingURL = twitterURL;
+    } else if (xURL?.length == 1) {
+    	workingURL = xURL;
+    } else {
+	    return;
+    }
 
     //Check if message has an embed
-    if (message.embeds.length == 0) return;
+    //if (message.embeds.length == 0) return;
 
     //Check if embed contains a video
-    if (!message.embeds[0].video) return;
+    //if (!message.embeds[0].video) return;
 
     //Destroy it and use VXTwitter!
-    const extractedURL = twitterURL[0].replace('https://twitter.com/', 'https://vxtwitter.com/');
+    const extractedURL = workingURL[0].replace('https://twitter.com/', 'https://vxtwitter.com/').replace('https://x.com/', 'https://vxtwitter.com/');
     await message.suppressEmbeds(true);
     await message.channel.send(`Original posted by <@${message.author.id}>\n${extractedURL}`);
 }
